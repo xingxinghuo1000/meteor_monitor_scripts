951bc8cc86dff0933c01f2a19ffc80521e23492c
xingxinghuo1000 <xingxinghuo@163.com>
2022-12-07 14:31:21 +0800

    add  run.sh

---
+++ detect_meteor/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+# usage:
+#    add this script to crontab, and run it every 5 minutes. 
+
+d=`date "+%Y%m%d_%H%M%S"`
+
+CNT=`ps aux| grep python | grep offline_detect_from_mp4.py| wc -l`
+if [ $CNT -eq 0 ]; then
+    /home/pi/py37env/bin/python  offline_detect_from_mp4.py > run.log 2>&1 &
+fi
+
+# delete old temp files
+find ./temp -type f -mtime +1 | xargs rm -f 
+
