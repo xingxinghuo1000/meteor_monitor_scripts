13fd9837d1cbbd8a3645f4171ce47664e7c36949
xingxinghuo1000@gmail.com <xingxinghuo1000@163.com>
2023-10-22 19:56:21 +0800

    work on ubuntu

--- detect_meteor/offline_detect_from_mp4.sh
+++ detect_meteor/offline_detect_from_mp4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-./venv/bin/python3 offline_detect_from_mp4.py 2>&1 | cronolog  run.%Y-%m-%d.log  &
+./venv/bin/python3 offline_detect_from_mp4.py 2>&1 | cronolog  logs/run.%Y-%m-%d.log  &
 
 
--- detect_meteor/stop.sh
+++ detect_meteor/stop.sh
@@ -1,5 +1,7 @@
+set -x
 
 ps aux| grep python3 | grep offline_detect_from_mp4.py | awk '{print $2}'  | xargs kill
+ps aux| grep ffmpeg 
 ps aux| grep ffmpeg | awk '{print $2}'  | xargs kill
 sleep 0.5
 
