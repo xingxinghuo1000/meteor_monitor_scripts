ffe6bbe957398e67d17ebbb83d5540cbed8d04d3
xingxinghuo1000 <xingxinghuo1000@163.com>
2023-12-10 21:11:03 +0800

    install bat

--- detect_meteor/install_dep.bat
+++ detect_meteor/install_dep.bat
@@ -2,3 +2,4 @@
 .\venv\Scripts\python -m pip install -U pip -i https://mirrors.aliyun.com/pypi/simple/
 .\venv\Scripts\python -m pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
  
+pause
