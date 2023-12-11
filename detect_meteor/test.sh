8449b30b07b4b28ed7c581d4470d3f59e310f9b6
xingxinghuo1000 <xingxinghuo1000@163.com>
2023-12-11 16:50:07 +0800

    README

--- detect_meteor/test.sh
+++ detect_meteor/test.sh
@@ -1,8 +1,27 @@
+set -x
 
 
+# first, if PYTHON_BIN is set in .config file, we will use this path
+if [ -f .config ]; then
+
+    PYTHON_BIN=`cat .config | grep PYTHON_BIN | awk -F'"' '{print $2}'`
+
+fi
 
 
-./venv/bin/python -m detect_meteor.py pytest util.py process_one_video.py --capture=no
+# then, if not set in .config, we will try to find PYTHON_BIN in venv
+if [[ "$PYTHON_BIN" == "" ]]; then
+    if [ -f ./venv/bin/python ]; then
+        PYTHON_BIN="./venv/bin/python"
+    fi
+fi
+
+if [ -z $PYTHON_BIN ]; then
+    echo "PYTHON_BIN not found"
+    exit 1
+fi
+
+$PYTHON_BIN -m pytest detect_meteor.py util.py process_one_video.py --capture=no
 
 
 
