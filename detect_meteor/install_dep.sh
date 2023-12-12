c2996eebaf725d33ea8b45673e03d78cd58aeba1
xingxinghuo1000 <xingxinghuo1000@163.com>
2023-12-12 20:22:36 +0800

    add config

--- detect_meteor/install_dep.sh
+++ detect_meteor/install_dep.sh
@@ -1,3 +1,6 @@
+set -x
 
-./venv/bin/python3 -m pip install -U pip   -i https://mirrors.aliyun.com/pypi/simple/
+PYTHON_BIN=`cat .config | grep PYTHON_BIN | awk -F"=" '{print $2}' | awk -F'"' '{print $2}'`
+
+$PYTHON_BIN -m pip install -U pip   -i https://mirrors.aliyun.com/pypi/simple/
 
