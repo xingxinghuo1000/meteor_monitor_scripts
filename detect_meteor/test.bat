8d4ee22a07302181cf4bf5ed6d7b5b9e58e67f4e
xingxinghuo1000 <xingxinghuo1000@163.com>
2023-12-30 20:29:05 +0800

    filter move to lib

--- detect_meteor/test.bat
+++ detect_meteor/test.bat
@@ -1,4 +1,4 @@
 
-.\venv\Scripts\python -m pytest util.py detect_meteor.py process_one_video.py --capture=no
+.\venv\Scripts\python -m pytest util.py filter_lib.py detect_meteor.py process_one_video.py --capture=no
 pause
 
--- detect_meteor/test.sh
+++ detect_meteor/test.sh
@@ -2,7 +2,7 @@ set -x
 
 PYTHON_BIN=`cat config.ini | grep PYTHON_BIN | awk -F'=' '{print $2}'`
 
-$PYTHON_BIN -m pytest detect_meteor.py util.py process_one_video.py --capture=no
+$PYTHON_BIN -m pytest detect_meteor.py util.py filter_lib.py process_one_video.py --capture=no
 
 
 
